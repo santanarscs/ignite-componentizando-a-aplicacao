f6b4457d2ecc354730a3ce62af266c5f0ee03cde
Raphael Santana <raphaelstn@gmail.com>
2021-03-22 10:55:52 -0300

    finish

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,40 @@
-export function Content() {
-  // Complete aqui
+import { MovieCard } from './MovieCard';
+
+interface Movie{
+  imdbID: string;
+  Title: string;
+  Poster: string;
+  Ratings: Array<{
+    Source: string;
+    Value: string;
+  }>;
+  Runtime: string;
+
+}
+interface Genre{
+  id: number;
+  title: string;
+}
+
+interface ContentProps {
+  movies: Movie[]
+  selectedGenre: Genre
+}
+
+export function Content({selectedGenre, movies}: ContentProps) {
+  return (
+    <div className="container">
+        <header>
+          <span className="category">Categoria:<span> {selectedGenre.title}</span></span>
+        </header>
+
+        <main>
+          <div className="movies-list">
+            {movies.map(movie => (
+              <MovieCard key ={movie.imdbID} title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
+            ))}
+          </div>
+        </main>
+      </div>
+  )
 }
